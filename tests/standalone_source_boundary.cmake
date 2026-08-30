63d0223905682d8b5adf5e33158ee0569733aad5
cshaxu <cshaxu@gmail.com>
2026-08-29 22:46:10 -0700

    Scan full CCPU manifest for BOP dispatch

--- tests/standalone_source_boundary.cmake
+++ tests/standalone_source_boundary.cmake
@@ -1,8 +1,16 @@
 set(standalone_sources
-    "${SOFTPC_SOURCE_DIR}/src/core/softpc/base/ccpu386/c_main.c"
     "${SOFTPC_SOURCE_DIR}/src/core/softpc/base/ccpu386/softpc_ccpu_facade.c"
     "${SOFTPC_SOURCE_DIR}/src/core/softpc_standalone_platform.c"
     "${SOFTPC_SOURCE_DIR}/src/core/softpc_machine.c")
+
+file(STRINGS "${SOFTPC_SOURCE_DIR}/src/core/softpc/base/ccpu386/c-files"
+    ccpu_source_names)
+foreach(name IN LISTS ccpu_source_names)
+    if(NOT name MATCHES "^p\\.")
+        list(APPEND standalone_sources
+            "${SOFTPC_SOURCE_DIR}/src/core/softpc/base/ccpu386/${name}")
+    endif()
+endforeach()
 
 foreach(source IN LISTS standalone_sources)
     file(READ "${source}" contents)
