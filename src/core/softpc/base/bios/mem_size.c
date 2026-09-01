8b72547c1716d03600240cff77c848a28878ce64
cshaxu <cshaxu@gmail.com>
2026-08-31 21:41:02 -0700

    M1 T1 S4 P2: restore original BIOS BOP services

--- src/core/softpc/base/bios/mem_size.c
+++ src/core/softpc/base/bios/mem_size.c
@@ -3,13 +3,39 @@
 /*
  * VPC-XT Revision 1.0
  *
- * Title        : memory_size
+ * Title	: memory_size
  *
- * Description  : Returns the memory size of the virtual PC memory
+ * Description	: Returns the memory size of the virtual PC memory
+ *
+ * Author	: Henry Nash
+ *
+ * Notes	: None
+ *
  */
 
+#ifdef SCCSID
+static char SccsID[]="@(#)mem_size.c	1.7 08/03/93 Copyright Insignia Solutions Ltd.";
+#endif
+
+#ifdef SEGMENTATION
+/*
+ * The following #include specifies the code segment into which this
+ * module will by placed by the MPW C compiler on the Mac II running
+ * MultiFinder.
+ */
+#include "SOFTPC_BIOS.seg"
+#endif
+
+
+/*
+ *    O/S include files.
+ */
+#include <stdio.h>
 #include TypesH
 
+/*
+ * SoftPC include files
+ */
 #include "xt.h"
 #include CpuH
 #include "bios.h"
@@ -18,8 +44,12 @@
 void memory_size()
 {
     word memory_size;
-
-    /* Read the BIOS data area: applications are allowed to change it. */
+    
+    /*
+     * Return the memory size in AX.  This is read in from the BIOS, as
+     * certain applications can write to this area.
+     */
     sas_loadw(MEMORY_VAR, &memory_size);
+    
     setAX(memory_size);
 }
