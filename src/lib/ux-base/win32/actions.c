47f8755ece07e63d9ca64cecfaadf42c8236a7bd
cshaxu <cshaxu@gmail.com>
2026-09-07 18:45:49 -0700

    M9 T42 S4 P35: remove legacy unified UX

--- src/lib/ux-base/win32/actions.c
+++ src/lib/ux-base/win32/actions.c
@@ -9,11 +9,11 @@ lib_u8 ux_win32_modifiers_from_key_state(void)
     lib_u8 modifiers = 0u;
 
     if ((GetKeyState(VK_CONTROL) & 0x8000) != 0)
-        modifiers |= UX_MODIFIER_CONTROL;
+        modifiers |= UX_HOTKEY_MODIFIER_CONTROL;
     if ((GetKeyState(VK_MENU) & 0x8000) != 0)
-        modifiers |= UX_MODIFIER_ALT;
+        modifiers |= UX_HOTKEY_MODIFIER_ALT;
     if ((GetKeyState(VK_SHIFT) & 0x8000) != 0)
-        modifiers |= UX_MODIFIER_SHIFT;
+        modifiers |= UX_HOTKEY_MODIFIER_SHIFT;
     return modifiers;
 }
 
@@ -22,11 +22,11 @@ lib_u8 ux_win32_modifiers_from_console_state(DWORD control_state)
     lib_u8 modifiers = 0u;
 
     if ((control_state & (LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED)) != 0u)
-        modifiers |= UX_MODIFIER_CONTROL;
+        modifiers |= UX_HOTKEY_MODIFIER_CONTROL;
     if ((control_state & (LEFT_ALT_PRESSED | RIGHT_ALT_PRESSED)) != 0u)
-        modifiers |= UX_MODIFIER_ALT;
+        modifiers |= UX_HOTKEY_MODIFIER_ALT;
     if ((control_state & SHIFT_PRESSED) != 0u)
-        modifiers |= UX_MODIFIER_SHIFT;
+        modifiers |= UX_HOTKEY_MODIFIER_SHIFT;
     return modifiers;
 }
 #endif
